e950a55f9a2cc992ad451f359c047144d54fee25
MD RASEL BABU <57211344+RASEL-BABU@users.noreply.github.com>
2019-11-30 21:46:56 +0600

    Add files via upload

---
+++ uriproblem.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+   double a,b,MEDIA;
+   scanf("%lf%lf",&a,&b);
+   a=a*3.5;
+   b=b*7.5;
+   MEDIA=(a+b)/11;
+   printf("MEDIA = %0.5lf\n",MEDIA);
+
+}
+//#include<stdio.h>
+main()
+{
+   double a,b,MEDIA;
+   scanf("%lf%lf",&a,&b);
+   a=a*3.5;
+   b=b*7.5;
+   MEDIA=(a+b)/11;
+   printf("MEDIA = %0.5lf\n",MEDIA);
+
+//}
