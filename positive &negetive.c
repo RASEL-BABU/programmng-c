50f0ff986f8e415dc020fcdc3a69f9b8c9363322
MD RASEL BABU <57211344+RASEL-BABU@users.noreply.github.com>
2019-12-07 00:33:56 +0600

    Add files via upload

---
+++ positive &negetive.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+   int num;
+   printf("enter the value:");
+   scanf("%d",&num);
+   if(num>0)
+
+      printf("positive");
+
+
+   else if(num<0)
+   printf("negetive");
+   else
+      printf("0");
+
+}
