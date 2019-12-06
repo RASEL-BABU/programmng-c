dfe102cc1fd7e76ce010984dd3e1e4091490f92c
MD RASEL BABU <57211344+RASEL-BABU@users.noreply.github.com>
2019-12-07 00:12:47 +0600

    Add files via upload

---
+++ studentmarking system.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main(){
+float mark;
+printf("ENTER THE MARK:");
+scanf("%f",&mark);
+if(mark>100||mark<0)
+   printf("INVALID MARK");
+else if(mark>=80&&mark<100)
+   printf("A+");
+else if(mark>=70&&mark<79)
+   printf("A");
+else if(mark>=60&&mark<69)
+   printf("A-");
+else if(mark>=50&&mark<59)
+   printf("B");
+else if(mark>=40&&mark<49)
+   printf("C");
+else if(mark>=33&&mark<39)
+   printf("D");
+   else
+      printf("FAIl");
+}
