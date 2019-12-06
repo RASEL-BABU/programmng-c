7d050f30d0c000498af81e9a971c801daf4525c3
MD RASEL BABU <57211344+RASEL-BABU@users.noreply.github.com>
2019-12-06 20:35:22 +0600

    Add files via upload

---
+++ .c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,max;
+printf("enter the value:");
+scanf("%d%d%d",&a,&b,&c);
+max=a;
+if (max<b)
+{
+   max=b;
+}
+if(max<c);
+{
+   max=c;
+
+}
+printf("the maximum is:%d",max);
+
+}
