64061ca04ad7eea5d6cbd2bc2db5bab830c98081
MD RASEL BABU <57211344+RASEL-BABU@users.noreply.github.com>
2019-12-07 21:00:28 +0600

    Add files via upload

---
+++ maximum of array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+   int num[100],n,i,max;
+   printf("Enter The Number:");
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+      scanf("%d",&num[i]);
+
+   }
+   max=num[0];
+   for(i=0;i<n;i++)
+   {
+      if(max<num[i]){
+         max=num[i];
+         }
+
+   }
+   printf("maximum:%d",max);
+
+}
