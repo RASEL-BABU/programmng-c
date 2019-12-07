04ed12e0c3c1f418feee1ae724ce7257f923c0e4
MD RASEL BABU <57211344+RASEL-BABU@users.noreply.github.com>
2019-12-07 14:45:21 +0600

    Add files via upload

---
+++ prime number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+   int num,count=0,i;
+   printf("enter the number:");
+   scanf("%d",&num);
+   for(i=2;i<num;i++)
+   {
+      if(num%i==0)
+      {
+         count++;
+         break;
+      }
+   }
+   if(count==0)
+      printf("prime number\n");
+   else
+      printf("not prime\n");
+}
